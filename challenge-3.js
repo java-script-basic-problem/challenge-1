cbb8057920c1047ad1b1a6f4cf7fe928c9fca641
alviasad0 <alviasad10@gmail.com>
2023-08-12 03:09:40 -0700

    added chanllenge-3

---
+++ challenge-3.js
@@ -0,0 +1,8 @@
+// Coding challenge #3: Print the multiplication table with 7
+
+// ans---------------------->
+
+for(let i = 1; i <=10; i++){
+    let table = "7 * " + i + " = " + i*7;
+    console.log(table)
+}
