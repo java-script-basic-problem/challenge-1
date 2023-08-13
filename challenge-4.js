ea3255205a1ce1e371b05e1c25767481aa151227
alviasad0 <alviasad10@gmail.com>
2023-08-13 08:47:33 -0700

    added challenge-5

---
+++ challenge-4.js
@@ -0,0 +1,21 @@
+// Coding challenge #4: Print all the multiplication tables with numbers from 1 to 10
+
+
+// ans-------------------->
+
+// for(let i = 1; i <= 10; i++)
+// {
+//     printTable(i);
+//      console.log("");
+// }
+
+function printTable(n)
+{
+    for(let i = 1; i <= 10; i++)
+    {
+        let row = n + " * " + i + " = " + n * i;
+         return;
+    }
+}
+ 
+const result = printTable
---
+++ challenge-5.js
@@ -0,0 +1,7 @@
+coding challenge 5 
+
+ let sum=0;
+ for(i=1;i<=10;i++){
+     sum+=i;
+ }
+ console.log(sum)
