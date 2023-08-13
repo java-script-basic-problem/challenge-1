78fe008bf0aa7ff1023674a528d9d53ba43b7fd0
alviasad0 <alviasad10@gmail.com>
2023-08-13 09:06:53 -0700

    added challenge-7

---
+++ challenge-7.js
@@ -0,0 +1,10 @@
+// Coding challenge #7: Calculate the sum of odd numbers greater than 10 and less than 30
+
+// ans------------------>
+
+let sum = 0;
+for(let i =11; i<=30; i+=2){
+    sum = sum+i;
+}
+
+console.log(sum)
